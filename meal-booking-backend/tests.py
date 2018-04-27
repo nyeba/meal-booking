0b29f7297ca2d3c7edaddd72158b710edf49eac8
nyeba oscar mathew <ryanrodneyosman@gmail.com>
2018-04-27 03:57:59 +0300

    commit this

---
+++ meal-booking-backend/tests.py
@@ -0,0 +1,9 @@
+from flask import flask 
+import unittest
+import admin
+
+
+class UserLogin(unittest.TestCase)
+    def user_must_login(self):
+        self.assertEual(email,result)
+        selfassertEual(password,result)
